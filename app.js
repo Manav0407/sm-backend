a262b890833ad390dc02ee997f842cc0c727b430
Manav0407 <128259533+Manav0407@users.noreply.github.com>
2024-04-13 01:20:21 +0530

    cors()

--- app.js
+++ app.js
@@ -14,16 +14,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use(cors({
-  origin:"http://localhost:3000",
-  methods:["GET","POST","PUT","DELETE"],
-  credentials:true,
-}));
+// app.use(cors({
+//   origin:"http://localhost:3000",
+//   methods:["GET","POST","PUT","DELETE"],
+//   credentials:true,
+// }));
 // app.use(cors({
 // origin:"https://sm-frontend-awez.vercel.app",
 // credentials:true,
 // }))
-// app.use(cors());
+app.use(cors());
 // app.options('*', cors())
 
 // app.use(function (req, res, next) {
